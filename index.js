43ef5e8fe9f8a35ead68ec6d946f0705a7034490
Aulia Rahman <auliarahman@delegasi.co>
2023-02-17 08:20:39 +0700

    add dc compose and update index.js

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 3000
 var os = require('os');
 
+var count = 0
+const version = process.env.VERSION || 'v1'
+
 app.get('/', (req, res) => {
-  console.log(`hello, we get request: ${req}`)
-  res.send(`Hello World! ${os.hostname()}`)
+  console.log(`hello we get request: ${req} - ${version} - count hit ${count}`)
+  count = count + 1
+  res.send(`Hello World! ${os.hostname()} - ${version} - count hit ${count}`)
 })
 
 app.listen(port, () => {
