e0a6ea711280168ea928c6d706ef3fa8b21c82c2
Aulia Rahman <auliarahman@delegasi.co>
2023-02-17 11:20:17 +0700

    feat: update v2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 var os = require('os');
 
 var count = 0
-const version = process.env.VERSION || 'v1'
+const version = process.env.VERSION || 'v2'
 
 app.get('/', (req, res) => {
   console.log(`hello we get request: ${req} - ${version} - count hit ${count}`)
