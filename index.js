7d481488db5787f0d001400246d6ada631c7848b
Aulia Rahman <auliarahman@delegasi.co>
2023-02-21 09:54:20 +0700

    use private registry

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 var os = require('os');
 
 var count = 0
-const version = process.env.VERSION || 'v3'
+const version = process.env.VERSION || 'v3 - private-registry '
 
 app.get('/', (req, res) => {
   console.log(`hello we get request: ${req} - ${version} - count hit ${count}`)
